d03a60563f4d9512707e5789cfd5a4622e39069d
oriakss <hellishsaiyan@outlook.com>
2024-10-07 08:55:09 +0300

    updated TestData

--- src/test/java/util/TestData.java
+++ src/test/java/util/TestData.java
@@ -3,9 +3,11 @@ package util;
 import by.clevertec.common.CakeType;
 import by.clevertec.domain.Cake;
 import by.clevertec.entity.CakeEntity;
+import org.junit.jupiter.params.provider.Arguments;
 
 import java.time.OffsetDateTime;
 import java.util.UUID;
+import java.util.stream.Stream;
 
 public class TestData {
 
@@ -13,17 +15,32 @@ public class TestData {
 
     public static Cake getCake() {
         return new Cake()
-                .setId(UUID.fromString("3d77ab90-b7bb-40f6-a64d-e1f484e928d8"))
-                .setTitle("Cake1")
+                .setId(UUID.randomUUID())
+                .setTitle("Cake")
                 .setCakeType(CakeType.BIG)
                 .setExpiredPeriod(EXPIRED_PERIOD);
     }
 
     public static CakeEntity getCakeEntity() {
         return new CakeEntity()
-                .setId(UUID.fromString("3d77ab90-b7bb-40f6-a64d-e1f484e928d8"))
-                .setTitle("Cake1")
-                .setCakeType(CakeType.BIG)
+                .setId(UUID.randomUUID())
+                .setTitle("Some Cake")
+                .setCakeType(CakeType.SMALL)
                 .setExpiredPeriod(EXPIRED_PERIOD);
+    }
+
+    public static Stream<Arguments> getArgumentsForTest() {
+        return Stream.of(
+                Arguments.of(
+                        UUID.fromString("3d77ab90-b7bb-40f6-a64d-e1f484e928d8"),
+                        getCake().setId(UUID.fromString("3d77ab90-b7bb-40f6-a64d-e1f484e928d8")),
+                        getCakeEntity().setId(UUID.fromString("3d77ab90-b7bb-40f6-a64d-e1f484e928d8"))
+                ),
+                Arguments.of(
+                        UUID.fromString("0b8d6dd9-1310-415c-9dbd-c623321836a5"),
+                        getCake().setId(UUID.fromString("0b8d6dd9-1310-415c-9dbd-c623321836a5")),
+                        getCakeEntity().setId(UUID.fromString("0b8d6dd9-1310-415c-9dbd-c623321836a5"))
+                )
+        );
     }
 }
